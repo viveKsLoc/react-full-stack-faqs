94bec1da08ac057331205e2d459ffd13158c02ff
Marek Sautter <marek.sautter@gmail.com>
2019-09-07 18:53:21 -0700

    Made OptionBar into its own component

---
+++ src/components/OptionBar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Navbar, Button, Badge } from "reactstrap";
+import sets from "../data/sets";
+import tags from "../data/tags";
+import types from "../data/types";
+
+export default function OptionBar({activeTab, setActiveTab}) {
+  const styles = {
+    buttonStyle : {
+      color: activeTab === "Sets" ? "white" : "black", 
+      textDecoration: "none"
+    },
+    buttonColorStyle : activeTab === "Sets" ? "secondary" : "link"
+  }
+  return (
+    <Navbar color="light" light expand="md">
+      <Button
+        style={{
+          color: activeTab === "Sets" ? "white" : "black",
+          textDecoration: "none"
+        }}
+        color={activeTab === "Sets" ? "secondary" : "link"}
+        onClick={() => setActiveTab("Sets")}
+      >
+        Sets{" "}
+        <Badge color={activeTab === "Sets" ? "light" : "secondary"}>
+          {sets.length}
+        </Badge>
+      </Button>{" "}
+      <Button
+        style={{
+          color: activeTab === "Types" ? "white" : "black",
+          textDecoration: "none"
+        }}
+        color={activeTab === "Types" ? "secondary" : "link"}
+        onClick={() => setActiveTab("Types")}
+      >
+        Types{" "}
+        <Badge color={activeTab === "Types" ? "light" : "secondary"}>
+          {types.length}
+        </Badge>
+      </Button>{" "}
+      <Button
+        style={{
+          color: activeTab === "Tags" ? "white" : "black",
+          textDecoration: "none"
+        }}
+        color={activeTab === "Tags" ? "secondary" : "link"}
+        onClick={() => setActiveTab("Tags")}
+      >
+        Tags{" "}
+        <Badge color={activeTab === "Tags" ? "light" : "secondary"}>
+          {tags.length}
+        </Badge>
+      </Button>
+    </Navbar>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import {
 
 import OptionCard from "./components/OptionCard";
 import DisplayCard from "./components/DisplayCard";
+import OptionBar from "./components/OptionBar";
 import sets from "./data/sets";
 import tags from "./data/tags";
 import types from "./data/types";
@@ -38,47 +39,7 @@ function App() {
       >
         <Row>
           <Col xs="4">
-            <Navbar color="light" light expand="md">
-              <Button
-                style={{
-                  color: activeTab === "Sets" ? "white" : "black",
-                  textDecoration: "none"
-                }}
-                color={activeTab === "Sets" ? "secondary" : "link"}
-                onClick={() => setActiveTab("Sets")}
-              >
-                Sets{" "}
-                <Badge color={activeTab === "Sets" ? "light" : "secondary"}>
-                  {sets.length}
-                </Badge>
-              </Button>{" "}
-              <Button
-                style={{
-                  color: activeTab === "Types" ? "white" : "black",
-                  textDecoration: "none"
-                }}
-                color={activeTab === "Types" ? "secondary" : "link"}
-                onClick={() => setActiveTab("Types")}
-              >
-                Types{" "}
-                <Badge color={activeTab === "Types" ? "light" : "secondary"}>
-                  {types.length}
-                </Badge>
-              </Button>{" "}
-              <Button
-                style={{
-                  color: activeTab === "Tags" ? "white" : "black",
-                  textDecoration: "none"
-                }}
-                color={activeTab === "Tags" ? "secondary" : "link"}
-                onClick={() => setActiveTab("Tags")}
-              >
-                Tags{" "}
-                <Badge color={activeTab === "Tags" ? "light" : "secondary"}>
-                  {tags.length}
-                </Badge>
-              </Button>
-            </Navbar>
+            <OptionBar activeTab={activeTab} setActiveTab={setActiveTab} />
             <TabContent activeTab={activeTab}>
               <TabPane tabId="Sets">
                 <Scrollbar
